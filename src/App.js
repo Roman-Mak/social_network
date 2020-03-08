2b6f1420322d4c44f68957225718842d8d7fb0f8
Roman-Mak <meller1758@gmail.com>
2020-03-09 00:57:12 +0300

    social_network12

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Content from "./components/Content";
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <Content/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Profile from "./content_components/Profile";
+
+const Content = () => {
+    return <div className='content'>
+        <Profile/>
+    </div>
+}
+
+export default Content;
