c5963957ea24844c7ab48a173e3210b340177f5b
Roman-Mak <meller1758@gmail.com>
2020-07-19 01:18:34 +0300

    94 react.lazy and react.suspense

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import {Route, withRouter} from "react-router-dom";
+import {Route, Switch, withRouter} from "react-router-dom";
 import ProfileContainer from "../content_components/Profile/ProfileContainer";
-import MessagesContainer from "../content_components/Messages/MessagesContainer";
-import Friends from "../content_components/Friends/Friends";
-import News from "../content_components/News/News";
-import Music from "../content_components/Music/Music";
-import Settings from "../content_components/Settings/Settings";
+// import MessagesContainer from "../content_components/Messages/MessagesContainer";
+// import Friends from "../content_components/Friends/Friends";
+// import News from "../content_components/News/News";
+// import Music from "../content_components/Music/Music";
+// import Settings from "../content_components/Settings/Settings";
 import UsersContainer from "../content_components/Users/UsersContainer";
 import Login from "../content_components/Login/Login";
+import Preloader from "../common/Preloader/Preloader";
+
+const MessagesContainer = React.lazy(() => import("../content_components/Messages/MessagesContainer"));
+const Friends = React.lazy(() => import("../content_components/Friends/Friends"));
+const News = React.lazy(() => import("../content_components/News/News"));
+const Music = React.lazy(() => import("../content_components/Music/Music"));
+const Settings = React.lazy(() => import("../content_components/Settings/Settings"));
 
 export const PROFILE_PATH = "/profile";
 export const MESSAGES_PATH = "/messages";
@@ -21,14 +28,18 @@ export const LOGIN_PATH = "/login";
 const Routes = () => {
     return (
         <div className="content">
-            <Route path={PROFILE_PATH + "/:userId?"} render={() => <ProfileContainer/>}/>
-            <Route path={MESSAGES_PATH} render={() => <MessagesContainer/>}/>
-            <Route path={FRIENDS_PATH} render={() => <Friends/>}/>
-            <Route path={NEWS_PATH} render={() => <News/>}/>
-            <Route path={MUSIC_PATH} render={() => <Music/>}/>
-            <Route path={SETTINGS_PATH} render={() => <Settings/>}/>
-            <Route path={USERS_PATH} render={() => <UsersContainer/>}/>
-            <Route path={LOGIN_PATH} render={() => <Login/>}/>
+            <React.Suspense fallback={<Preloader/>}>
+                <Switch>
+                    <Route path={PROFILE_PATH + "/:userId?"} render={() => <ProfileContainer/>}/>
+                    <Route path={MESSAGES_PATH} render={() => <MessagesContainer/>}/>
+                    <Route path={FRIENDS_PATH} render={() => <Friends/>}/>
+                    <Route path={NEWS_PATH} render={() => <News/>}/>
+                    <Route path={MUSIC_PATH} render={() => <Music/>}/>
+                    <Route path={SETTINGS_PATH} render={() => <Settings/>}/>
+                    <Route path={USERS_PATH} render={() => <UsersContainer/>}/>
+                    <Route path={LOGIN_PATH} render={() => <Login/>}/>
+                </Switch>
+            </React.Suspense>
         </div>
     );
 };
